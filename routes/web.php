8f54f05a45373f187160a54eea7ca42a85f8defd
rizkyyfaauzi21 <muhammadrizkyfauzi061102@gmail.com>
2024-02-20 11:27:01 +0700

    Tugas Router dan Parameter

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,49 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// // Soal 1
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
+
+// // Soal 2
+// Route::get('/world', function () {
+//     return 'World';
+// });
+
+// Route::get('/datang', function () {
+//     return 'Selamat Datang';
+// });
+
+// Route::get('/about', function () {
+//     return '2241720221, Muhammad Rizky Fauzi';
+// });
+
+// // Soal 3
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya ' . $name;
+// });
+
+// // Soal 4
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+// });
+
+// Route::get('/articles/{id}', function ($id) {
+//     return 'Halaman artikel dengan ID ' . $id;
+// });
+
+// // Soal 5
+// Route::get('/user/{name?}', function ($name = null) {
+//     return 'Nama saya ' . $name;
+// });
+
+// // Soal 6
+// Route::get('/user/{name?}', function ($name = 'John') {
+//     return 'Nama saya ' . $name;
+// });
+
+// Soal 7
+Route::get('/user/profile', function () {
+})->name('profile');
